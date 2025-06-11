9d239b73de85d7d5c8182afd880e4d91691fcdb4
eemmmmg <mark.golden@bitcoin.com>
2025-06-10 22:46:59 -0500

    Switch to 7b

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -46,7 +46,7 @@ return {
     {
         "David-Kunz/gen.nvim",
         opts = {
-            model = "qwen2.5-coder:14b", -- The default model to use.
+            model = "qwen2.5-coder:7b", -- The default model to use.
             quit_map = "q", -- set keymap to close the response window
             retry_map = "<c-r>", -- set keymap to re-send the current prompt
             accept_map = "<c-cr>", -- set keymap to replace the previous selection with the last result
